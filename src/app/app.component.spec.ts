121ff2e3c2c1dcedfe064b7f48d0b4941c187c9b
Abdouni Abdelkarim <abdounikarim@gmail.com>
2025-01-24 19:23:38 +0100

    Test app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'poc-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('poc-angular');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, poc-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello from Angular!👋');
   });
 });
